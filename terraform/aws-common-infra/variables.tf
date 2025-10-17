c4d3894f38aeb1d995e9204262341fe7da172b52
sanket daule <sanketdaule25@gmail.com>
2025-10-17 21:19:41 +0530

    modified vpc module

--- terraform/aws-common-infra/variables.tf
+++ terraform/aws-common-infra/variables.tf
@@ -4,9 +4,41 @@ variable "region" {
 }
 
 variable "environment" {
-  type    = string
+  type = string
 }
 
 variable "project" {
   type    = string
+  default = "cloud-native-python-ecs"
+}
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "public_subnet_map" {
+  description = "Map of public subnets"
+  type = map(object({
+    cidr_block = string
+    az         = string
+    tags       = map(string)
+  }))
+}
+
+variable "private_subnet_map" {
+  description = "Map of private subnets"
+  type = map(object({
+    cidr_block = string
+    az         = string
+    tags       = map(string)
+  }))
 }
--- terraform/aws-common-infra/vpc.tf
+++ terraform/aws-common-infra/vpc.tf
@@ -1,51 +1,16 @@
 module "network" {
   source = "./modules/vpc"
 
-  vpc_cidr_block       = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  region               = "ap-south-1"
+  vpc_cidr_block       = var.vpc_cidr_block
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+  region               = var.region
+
+  public_subnet_map    = var.public_subnet_map
+  private_subnet_map   = var.private_subnet_map
 
   tags = {
     Environment = "dev"
     Project     = "ecs-app"
-  }
-
-  public_subnet_map = {
-    "public-a" = {
-      cidr_block = "10.0.1.0/24"
-      az         = "ap-south-1a"
-      tags       = {
-        Name = "public-subnet-a"
-        Type = "public"
-      }
-    }
-    "public-b" = {
-      cidr_block = "10.0.2.0/24"
-      az         = "ap-south-1b"
-      tags       = {
-        Name = "public-subnet-b"
-        Type = "public"
-      }
-    }
-  }
-
-  private_subnet_map = {
-    "private-a" = {
-      cidr_block = "10.0.3.0/24"
-      az         = "ap-south-1a"
-      tags       = {
-        Name = "private-subnet-a"
-        Type = "private"
-      }
-    }
-    "private-b" = {
-      cidr_block = "10.0.4.0/24"
-      az         = "ap-south-1b"
-      tags       = {
-        Name = "private-subnet-b"
-        Type = "private"
-      }
-    }
   }
 }
