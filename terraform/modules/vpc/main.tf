e4a5aafc98d2264fbad2ddbb8d0fbc697443b561
SanketDaule <sanketdaule25@gmail.com>
2025-10-22 14:52:59 +0530

    gateway endpoints for s3 and dynamodb

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -68,3 +68,65 @@ resource "aws_route_table_association" "private_rt_association" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+#Security Group for Interface Endpoints 
+resource "aws_security_group" "vpc_endpoint" {
+  name        = "${var.name_prefix}-vpc-endpoint-sg"
+  description = "Allow ECS tasks to reach VPC Interface Endpoints (443)"
+  vpc_id      = aws_vpc.this.id
+  tags        = merge(var.tags, { Name = "${var.name_prefix}-vpc-endpoint-sg" })
+
+  # Egress: allow responses
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Ingress 443 from ECS Task SG (preferred)
+resource "aws_security_group_rule" "vpce_ingress_from_tasks" {
+  count                    = var.endpoint_allowed_sg_id != null ? 1 : 0
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.vpc_endpoint.id
+  source_security_group_id = var.endpoint_allowed_sg_id
+}
+
+#Ingress 443 from cidrs
+#Fallback if task sg is yet to be created
+locals {
+  endpoint_cidrs = length(var.endpoint_allowed_cidrs) > 0 ? var.endpoint_allowed_cidrs : [aws_vpc.this.cidr_block]
+}
+
+resource "aws_security_group_rule" "vpce_ingress_from_cidrs" {
+  count             = var.endpoint_allowed_sg_id == null ? 1 : 0
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_endpoint.id
+  cidr_blocks       = local.endpoint_cidrs
+}
+
+#Gateway Endpoints (S3, DynamoDB)
+resource "aws_vpc_endpoint" "s3" {
+  count             = var.create_gateway_endpoints && var.enable_s3_gateway_endpoint ? 1 : 0
+  vpc_id            = aws_vpc.ecs_vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+  tags              = merge(var.tags, { Name = "${var.name_prefix}-s3-gw-endpoint" })
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  count             = var.create_gateway_endpoints && var.enable_dynamodb_gateway_endpoint ? 1 : 0
+  vpc_id            = aws_vpc.ecs_vpc.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.dynamodb"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+  tags              = merge(var.tags, { Name = "${var.name_prefix}-dynamodb-gw-endpoint" })
+}
